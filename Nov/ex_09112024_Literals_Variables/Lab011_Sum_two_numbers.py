cbab906fe65c40471b2bf94b9108047fc8173f91
Iyappan <Iyappan@zeesoft.com>
2024-11-11 18:55:18 +0530

    Uploaded 9th Nov Lab

--- Nov/ex_09112024_Literals_Variables/Lab011_Sum_two_numbers.py
+++ Nov/ex_09112024_Literals_Variables/Lab011_Sum_two_numbers.py
@@ -1,5 +1,7 @@
 num1=int(input("Enter the number1:"))
 num2=int(input("Enter the number2:"))
+#num1=float(input("Enter the number1:"))
+#num2=float(input("Enter the number2:"))
 sum=num1 + num2
 sub=num1 - num2
 mul=num1 * num2
