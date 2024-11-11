e75457ae655339fae6da8e748bdd610a203b09bb
Iyappan <Iyappan@zeesoft.com>
2024-11-11 22:58:29 +0530

    Uploaded 9th Nov Lab

---
+++ Nov/ex_09112024_Literals_Variables/Task 0911_3number_input.py
@@ -0,0 +1,13 @@
+number1 = int(input("Enter the value of number1"))
+number2 = int(input("Enter the value of number2"))
+number3 = int(input("Enter the value of number3"))
+
+sum_result = number1 + number2 + number3
+sub_result = number1 - number2 - number3
+mul_result = number1 * number2 * number3
+div_result = number1 / number2 / number3
+
+print("The sum value is:", sum_result)
+print("The sub value is:", sub_result)
+print("The mul value is:", mul_result)
+print("The div value is:", div_result)
---
+++ Nov/ex_09112024_Literals_Variables/Task2 091124_quotient_remainder.py
@@ -0,0 +1,10 @@
+# Taking user input for two number
+dividend = int(input("Enter the dividend: "))
+divisor = int(input("Enter the divisor: "))
+
+quotient = dividend / divisor   # Floor division for quotient
+remainder = dividend % divisor   # Modulus for remainder
+
+ # Printing the results
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
